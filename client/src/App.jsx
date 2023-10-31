1ca313c827f2444b2c9a439c1624f76c1837075b
gökhan <eiengokhan34@gmail.com>
2023-10-31 17:53:56 +0300

    categories and products completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,18 @@
-import "./App.css";
+import Categories from "./components/Categories/Categories";
 import Footer from "./components/Layout/Footer/Footer";
 import Header from "./components/Layout/Header/Header";
 import Policy from "./components/Layout/Policy/Policy";
-import Slider from "./components/Layout/Slider/Slider";
+import Products from "./components/Products/Products";
+import Slider from "./components/Slider/Slider";
+import "./App.css";
 
 function App() {
   return (
     <>
       <Header />
       <Slider />
+      <Categories />
+      <Products />
       <Policy />
       <Footer />
     </>
