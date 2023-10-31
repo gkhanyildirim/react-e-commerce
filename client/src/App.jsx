f7f38bc3a22d51d07f49531e4c107750e235fcbb
gökhan <eiengokhan34@gmail.com>
2023-10-31 15:45:54 +0300

    policy and footer completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import Policy from "./components/Policy/Policy";
 
 function App() {
   return (
     <>
       <Header />
       <h1>Hello</h1>
+      <Policy />
+      <Footer />
     </>
   );
 }
